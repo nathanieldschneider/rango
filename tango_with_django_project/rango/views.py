3ada489fea2b6e7581e906870ce3ecea750030f2
unknown <nathanieldanielschneider@gmail.com>
2020-02-18 02:45:55 +0000

    Chapter 9 halfway.

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,7 +6,10 @@ from rango.forms import CategoryForm
 from django.shortcuts import redirect
 from rango.forms import PageForm
 from rango.forms import UserForm, UserProfileForm
-
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import redirect
 from django.http import HttpResponse
 
 
@@ -122,3 +125,23 @@ def register(request):
                   context={'user_form': user_form,
                            'profile_form': profile_form,
                            'registered': registered})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
